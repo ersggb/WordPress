c70521eb889fb6bac1e9cd51e9fc208039daf13d
Emma Schiermeier <ersggb@mail.missouri.edu>
2017-08-15 09:43:49 -0500

    Footer revised without links

--- wp-content/themes/twentyeleven/footer.php
+++ wp-content/themes/twentyeleven/footer.php
@@ -25,7 +25,10 @@
 
 			<div id="site-generator">
 				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+				<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+				-->
+				<a>copyright 2017 MSPD | privacy | bid opportunities<br>Office of  State Public Defender - Woodrail Centre - 1000 West Nifong, Building 7, Suite 100 - Columbia, MO 65203 <br>phone: 573-777-9977 email: public.defender@mspd.mo.gov</a>
+				
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
